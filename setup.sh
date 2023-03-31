585169d9b0f641d5d61da073e067b55f23d97d7a
kangtw6511 <115384348+kangtw6511@users.noreply.github.com>
2023-03-31 23:23:43 +0900

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -9,33 +9,34 @@ pkg upgrade -y
 pkg install -y sudo nano drm-kmod xorg firefox noto-kr lightdm-gtk-greeter xfce xfce4-goodies dbus fcitx5-gtk fcitx5-configtool ko-fcitx5-hangul wifimgr
 
 # Configure system settings
-echo 'kld_list="i915kms"' >> /boot/loader.conf
-sysrc -f /etc/rc.conf lightdm_enable="YES"
-sysrc -f /etc/rc.conf dbus_enable="YES"
-sysrc -f /etc/rc.conf fcitx_enable="YES"
-sysrc -f /etc/rc.conf fcitx_program_variable="FCITX5"
-sysrc -f /etc/rc.conf fcitx_flags="-d"
+echo 'kld_list="i915kms"' >> /boot/loader.conf.local
+sysrc kld_list+="i915kms"
+sysrc background_dhclient="YES"
+sysrc lightdm_enable="YES"
+sysrc dbus_enable="YES"
+sysrc fcitx_enable="YES"
+sysrc fcitx_program_variable="FCITX5"
+sysrc fcitx_flags="-d"
 
 # Configure sudo access for the wheel group
-pw groupmod wheel -m kang
-echo '%wheel ALL=(ALL) ALL' > /usr/local/etc/sudoers.d/wheel
+sed -i '' 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /usr/local/etc/sudoers
 
-# Add user to video group
+# Add user to wheel and video groups
 pw groupmod video -m kang
+pw groupmod wheel -m kang
 
 # Configure network settings
-sysrc -f /etc/rc.conf wlans_iwm0="wlan0"
-sysrc -f /etc/rc.conf ifconfig_wlan0="WPA DHCP"
+sysrc wlans_iwn0="wlan0"
+sysrc ifconfig_wlan0="WPA SYNCDHCP"
 
 # Set audio settings
 mixer mic 50:50
 mixer vol 95:95
 
 # Configure input method
-echo 'GTK_IM_MODULE=fcitx5' >> /etc/env.d/99fcitx5
-echo 'QT_IM_MODULE=fcitx5' >> /etc/env.d/99fcitx5
-echo 'XMODIFIERS="@im=fcitx5"' >> /etc/env.d/99fcitx5
-echo 'export GTK_IM_MODULE QT_IM_MODULE XMODIFIERS' >> /etc/profile
+echo 'export GTK_IM_MODULE=fcitx' >> /etc/profile
+echo 'export QT_IM_MODULE=fcitx' >> /etc/profile
+echo 'export XMODIFIERS="@im=fcitx"' >> /etc/profile
 
 # Configure locale
 echo 'ko_KR.UTF-8 UTF-8' >> /etc/locale.gen
@@ -43,26 +44,30 @@ locale-gen
 echo 'LANG=ko_KR.UTF-8' > /etc/locale.conf
 
 # Configure virtual terminal type
-sysrc -f /etc/rc.conf kern.vty="vt"
+sysrc kern.vty="vt"
 
 # Add /proc filesystem to /etc/fstab
 echo 'proc /proc procfs rw 0 0' >> /etc/fstab
 
 # Firefox optimization
 # Set the following sysctl values to optimize Firefox performance
-sysctl net.inet.tcp.sendbuf_max=2097152
-sysctl net.inet.tcp.recvbuf_max=2097152
+sysctl net.inet.tcp.delayed_ack=0
+sysctl net.inet.tcp.soreceive_stream=1
+sysctl net.inet.tcp.sendbuf_max=16777216
+sysctl net.inet.tcp.recvbuf_max=16777216
+sysctl net.inet.tcp.recvspace=4096
+sysctl net.inet.tcp.sendpace=4096
 sysctl kern.ipc.shm_allow_removed=1
-sysctl kern.ipc.shmmax=1073741824
+sysctl kern.ipc.shmmax=67108864
 sysctl kern.ipc.shmall=32768
-sysctl net.local.stream.recvspace=8192
-sysctl net.local.stream.sendspace=8192
-sysctl kern.maxfiles=65536
-sysctl vfs.zfs.arc_max=1073741824
+sysctl vfs.zfs.arc_max=5368709120
 sysctl kern.geom.trim.enabled=1
 
+# Increase the maximum number of open files
+sysctl kern.maxfiles=65536
+
 # Disable debugging symbols
-rm -rf /usr/lib/debug/*
+rm -f /usr/lib/debug/*
 
 # Reboot the system
 reboot
