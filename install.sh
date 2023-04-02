81396cd0bef7a49ea202211420a874fb1b8f55ea
kangtw6511 <115384348+kangtw6511@users.noreply.github.com>
2023-04-03 00:58:39 +0900

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,21 +1,16 @@
 #!/bin/sh
 
 # Update FreeBSD system
-freebsd-update fetch install
-pkg update -f
+freebsd-update install
 pkg upgrade -y
 
 # Install necessary packages
-pkg install -y sudo nano drm-kmod xorg firefox xfce xfce4-goodies fcitx5 fcitx5-configtool ko-fcitx5-hangul wifimgr dsbmixer lightdm lightdm-gtk-greeter
+pkg install -y sudo nano xorg xfce4 firefox fcitx5 fcitx5-configtool ko-fcitx5-hangul lightdm lightdm-gtk-greeter wifimgr dsbmixer
 
 # Configure system settings
-echo 'kld_list="/boot/modules/i915kms.ko"' >> /etc/rc.conf
-echo 'background_dhclient="YES"' >> /etc/rc.conf
-echo 'dbus_enable="YES"' >> /etc/rc.conf
-echo 'lightdm_enable="YES"' >> /etc/rc.conf
-echo 'fcitx_enable="YES"' >> /etc/rc.conf
-echo 'fcitx_program_variable="FCITX5"' >> /etc/rc.conf
-echo 'fcitx_flags="-d"' >> /etc/rc.conf
+sysrc background_dhclient=YES
+sysrc dbus_enable=YES
+sysrc lightdm_enable=YES
 
 # Configure sudo access for the wheel group
 sed -i '' 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /usr/local/etc/sudoers
@@ -25,33 +20,22 @@ pw groupmod video -m kang
 pw groupmod wheel -m kang
 
 # Configure network settings
-echo 'wlans_iwn0="wlan0"' >> /etc/rc.conf
-echo 'ifconfig_wlan0="WPA SYNCDHCP"' >> /etc/rc.conf
+sysrc ifconfig_DEFAULT=DHCP
 
 # Set audio settings
 mixer mic 50:50
 mixer vol 95:95
 
 # Configure input method
-echo 'export GTK_IM_MODULE=fcitx' >> /etc/profile
-echo 'export QT_IM_MODULE=fcitx' >> /etc/profile
-echo 'export XMODIFIERS="@im=fcitx"' >> /etc/profile
+echo 'export GTK_IM_MODULE=fcitx5 QT_IM_MODULE=fcitx5 XMODIFIERS="@im=fcitx5"' >> /etc/profile
 
 # Configure locale
 echo 'ko_KR.UTF-8 UTF-8' >> /etc/locale.gen
 locale-gen
 echo 'LANG=ko_KR.UTF-8' > /etc/locale.conf
 
-# Configure virtual terminal type
-echo 'kern.vty="vt"' >> /etc/sysctl.conf
-
-# Add /proc filesystem to /etc/fstab
-echo 'proc /proc procfs rw 0 0' >> /etc/fstab
-
-# Set up LightDM GTK greeter
-echo '[Seat:*]' > /usr/local/etc/lightdm/lightdm.conf
-echo 'greeter-session=lightdm-gtk-greeter' >> /usr/local/etc/lightdm/lightdm.conf
-echo 'user-session=xfce' >> /usr/local/etc/lightdm/lightdm.conf
+# Set up graphical drivers for Intel graphics
+sysrc kld_list="/boot/modules/i915kms.ko"
 
 # Reboot the system
 echo "The system will now reboot..."
