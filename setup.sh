6eacb0b679c86dacc6450c79fe8f422882cf4b04
kangtw6511 <115384348+kangtw6511@users.noreply.github.com>
2023-04-07 20:01:34 +0900

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -6,11 +6,10 @@ pkg update -f
 pkg upgrade -y
 
 # Install necessary packages
-pkg install -y sudo nano drm-kmod xorg firefox noto-kr lightdm-gtk-greeter xfce xfce4-goodies dbus fcitx5-gtk fcitx5-configtool ko-fcitx5-hangul wifimgr
+pkg install -y sudo nano xorg firefox ko-nanumfonts noto-kr lightdm-gtk-greeter xfce xfce4-goodies dbus fcitx5-gtk fcitx5-configtool ko-fcitx5-hangul wifimgr dsbmixer
 
 # Configure system settings
-echo 'i915kms_load="YES"' >> /boot/loader.conf
-sysrc -f /etc/rc.conf kld_list+="i915kms"
+echo 'kld_list="i915kms"' >> /etc/rc.conf
 sysrc -f /etc/rc.conf background_dhclient="YES"
 sysrc -f /etc/rc.conf lightdm_enable="YES"
 sysrc -f /etc/rc.conf dbus_enable="YES"
@@ -22,12 +21,13 @@ sysrc -f /etc/rc.conf fcitx_flags="-d"
 sed -i '' 's/# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /usr/local/etc/sudoers
 
 # Add user to wheel and video groups
-pw groupmod video -m kang
-pw groupmod wheel -m kang
+user=$(whoami)
+pw groupmod video -m "$user"
+pw groupmod wheel -m "$user"
 
 # Configure network settings
-sysrc -f /etc/rc.conf wlans_iwn0="wlan0"
-sysrc -f /etc/rc.conf ifconfig_wlan0="WPA SYNCDHCP"
+sysrc -f /etc/rc.conf wlans_iwm0="wlan0"
+sysrc -f /etc/rc.conf ifconfig_wlan0="DHCP WPA"
 
 # Set audio settings
 mixer mic 50:50
@@ -43,11 +43,9 @@ echo 'ko_KR.UTF-8 UTF-8' >> /etc/locale.gen
 locale-gen
 echo 'LANG=ko_KR.UTF-8' > /etc/locale.conf
 
-# Configure virtual terminal type
-sysrc -f /etc/rc.conf kern.vty="vt"
-
-# Add /proc filesystem to /etc/fstab
-echo 'proc /proc procfs rw 0 0' >> /etc/fstab
+# Add /proc filesystem to /etc/fstab if it is not already there
+grep -q 'proc /proc' /etc/fstab || echo 'proc /proc procfs rw 0 0' >> /etc/fstab
 
 # Reboot the system
 reboot
+
